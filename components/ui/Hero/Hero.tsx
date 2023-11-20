95a4d9cd0e74eb4b8e5f2404ce527aa08ab509b4
Zach Asher <zach.asher@hotmail.co.uk>
2023-11-20 15:58:16 +0200

    Added hero section with upload

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='bg-red-500'>Hero</div>
+    <div className='flex flex-col items-center justify-center bg-white p-4'>
+      <h1 className='text-center text-black text-5xl py-4'>Bank Statment Converter</h1>
+      <p className='text-center text-black pb-4'>Convert your bank statements to Excel (XLS) or CSV</p>
+      <div className='bg-green-300 px-80 py-20 border-2 border-dashed border-green-600 rounded-xl'>
+        <div className='bg-green-600 px-20 py-6 rounded-full'>Upload File</div>
+      </div>
+    </div>
   )
 }
 
