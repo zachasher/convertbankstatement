29afad55ddbf64eda61ef349382334929296d3d6
Zach Asher <zach.asher@hotmail.co.uk>
2023-11-21 17:09:43 +0200

    Added hover to upload button

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
       <h1 className='text-center text-black text-5xl py-4 font-bold'>Bank Statment Converter</h1>
       <p className='text-center text-black pb-4'>Convert your bank statements to Excel (XLS) or CSV</p>
       <div className='bg-green-200 px-80 py-20 border-2 border-dashed border-green-600 rounded-xl mb-8'>
-        <div className='bg-green-600 px-20 py-6 rounded-full'>Upload File</div>
+        <div className='bg-green-600 px-20 py-6 rounded-full hover:bg-green-500 cursor-pointer'>Upload File</div>
       </div>
     </div>
   )
